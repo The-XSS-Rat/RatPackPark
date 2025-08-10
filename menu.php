068e8a2c705148052636b09b1337b566063d1757
The-XSS-Rat <pcwthijs@gmail.com>
2025-08-10 17:54:46 +0200

    Add maintenance module and auto-login on registration

--- menu.php
+++ menu.php
@@ -6,6 +6,7 @@ $menu_items = [
     ['label' => 'Tickets', 'url' => 'tickets.php', 'roles' => [1, 2, 3]],
     ['label' => 'My Roster', 'url' => 'my_roster.php', 'roles' => [1, 2, 4]],
     ['label' => 'Analytics', 'url' => 'analytics.php', 'roles' => [1]],
+    ['label' => 'Maintenance', 'url' => 'maintenance.php', 'roles' => [1, 2]],
     ['label' => 'Report a problem', 'url' => 'problem.php', 'roles' => [1,2,4]],
     ['label' => 'Admin problem overview', 'url' => 'admin_problem.php', 'roles' => [1]],
     ['label' => 'Logout', 'url' => 'logout.php', 'roles' => [1,2,3,4]],
